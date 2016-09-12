6e5cec762bad5d730edc7cb2304a9e8108ff7886
Steven <nzlili2015@gmail.com>
2016-09-12 21:04:25 +1200

    Insert back-end code

---
+++ knockoutJsDemo/Controllers/MealsController.cs
@@ -0,0 +1,33 @@
+﻿using knockoutJsDemo.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace knockoutJsDemo.Controllers
+{
+    public class MealsController : ApiController
+    {
+        Meal[] products = new Meal[]
+        {
+            new Meal { Id = 0, Name = "Standard (sandwich)", Price = 0 },
+            new Meal { Id = 1, Name = "Premium (lobster)", Price = 34.95M },
+            new Meal { Id = 2, Name = "Ultimate (whole zebra)", Price = 290M }
+        };
+
+        public IEnumerable<Meal> GetAllProducts()
+        {
+            return products;
+        }
+
+        public IHttpActionResult GetProduct(int id)
+        {
+            var product = products.FirstOrDefault((p) => p.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return Ok(product);
+        }
+
+    }
+}
---
+++ knockoutJsDemo/Models/Meal.cs
@@ -0,0 +1,10 @@
+﻿
+namespace knockoutJsDemo.Models
+{
+    public class Meal
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+    }
+}
