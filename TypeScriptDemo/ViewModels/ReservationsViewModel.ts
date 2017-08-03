fbee3cfc01cd49a75461ebf1d4cf647ace639d67
LL <52593730@qq.com>
2017-08-03 23:19:26 +1200

    refactor

--- TypeScriptDemo/ViewModels/ReservationsViewModel.ts
+++ TypeScriptDemo/ViewModels/ReservationsViewModel.ts
@@ -8,16 +8,21 @@ export class ReservationsViewModel {
     public totalSurcharge: KnockoutComputed<any>;
 
     constructor(mappedMeals: any) {
-        var self = this;
-
         this.availableMeals = ko.observableArray([]);
         this.availableMeals(mappedMeals);
 
+        // Load data
+        this.load();
+    }
+
+    public load = (): void => {
         // Editable data
         this.seats = ko.observableArray([
             new SeatReservation("Steve", this.availableMeals()[0]),
             new SeatReservation("Bert", this.availableMeals()[0])
         ]);
+
+        var self = this;
 
         // Computed data
         this.totalSurcharge = ko.computed(function () {
@@ -28,11 +33,11 @@ export class ReservationsViewModel {
         });
     }
 
-    public addSeat = () => {
+    public addSeat = (): void => {
         this.seats.push(new SeatReservation("", this.availableMeals()[0]));
     }
 
-    public removeSeat = (seat: SeatReservation) => {
+    public removeSeat = (seat: SeatReservation): void => {
         this.seats.remove(seat);
     }
 }
